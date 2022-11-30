3d0a4cdb3d793b0290e8be6559300818da997984
esuzero0917 <42059701+esuzero0917@users.noreply.github.com>
2022-11-30 22:43:06 +0900

    Chage wall 3DObject

--- Assets/Scripts/Shot/Shots/TripleShot.cs
+++ Assets/Scripts/Shot/Shots/TripleShot.cs
@@ -10,8 +10,8 @@ public class TripleShot : Shot
         player = GameObject.FindWithTag("Player");
         Quaternion rotate = Quaternion.Euler(0, 10, 0);
 
-        Instantiate(gameObject, player.transform.position  + new Vector3(0, offsetY, 0), player.transform.rotation * Quaternion.Inverse(rotate));
-        Instantiate(gameObject, player.transform.position  + new Vector3(0, offsetY, 0), player.transform.rotation * rotate);
-        Instantiate(gameObject, player.transform.position  + new Vector3(0, offsetY, 0), player.transform.rotation);
+        Instantiate(gameObject, player.transform.localPosition, player.transform.rotation * Quaternion.Inverse(rotate));
+        Instantiate(gameObject, player.transform.localPosition, player.transform.rotation * rotate);
+        Instantiate(gameObject, player.transform.localPosition, player.transform.rotation);
     }
 }
