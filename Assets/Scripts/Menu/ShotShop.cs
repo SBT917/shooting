fcb79ffd74ee9091fe8ef903bcde416457e29a4a
esuzero0917 <42059701+esuzero0917@users.noreply.github.com>
2023-01-11 03:20:47 +0900

    ショップのレアリティによる確率抽選の実装

--- Assets/Scripts/Menu/ShotShop.cs
+++ Assets/Scripts/Menu/ShotShop.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class ShotShop : MonoBehaviour
 {
     [SerializeField]private Shot[] shots;
+    [SerializeField]private Rarity[] rarities;
     [SerializeField]private ShotSlot[] shotSlots;
     [SerializeField]private ShopObject shopObject;
     [SerializeField]private List<Shot> shopLineup;
+    private Dictionary<string, List<Shot>> shotBox = new Dictionary<string, List<Shot>>();
     
+    void Awake()
+    {
+        foreach(Rarity rarity in rarities){
+            shotBox.Add(rarity.name, new List<Shot>());
+        }
+
+        foreach(Shot shot in shots){
+            shotBox[shot.shotData.rarity.name].Add(shot);
+        }
+
+    }
+
     public void DrawingShop()
     {   
         if(shopLineup != null){
@@ -19,12 +33,38 @@ public class ShotShop : MonoBehaviour
         }
 
         while(shopLineup.Count < 3){
-            int value = Random.Range(0, shots.Length);
-                if(!shopLineup.Contains(shots[value]) && shots[value] != shotSlots[0].shot && shots[value] != shotSlots[1].shot){
-                    ShopObject so = Instantiate<ShopObject>(shopObject, transform);
-                    so.shot = shots[value];
-                    shopLineup.Add(so.shot);
-                }  
-            }
+            List<Shot> box = shotBox[RarityLottery()];
+            int value = Random.Range(0, box.Count);
+            Shot choosedShot = box[value];
+            if(!shopLineup.Contains(choosedShot) && choosedShot != shotSlots[0].shot && choosedShot != shotSlots[1].shot){
+                ShopObject so = Instantiate<ShopObject>(shopObject, transform);
+                so.shot = choosedShot;
+                shopLineup.Add(so.shot);
+            }  
         }
     }
+
+    private string RarityLottery()
+    {
+        float total = 0;
+
+        foreach(Rarity rarity in rarities){
+            total += rarity.weight;
+        }
+
+        float randomValue = Random.value * total;
+
+        foreach(Rarity rarity in rarities){
+            if(randomValue < rarity.weight){
+                return rarity.name;
+            }
+            else{
+                randomValue -= rarity.weight;
+            }
+        }
+
+        return rarities[rarities.Length - 1].name;
+    }
+}
+
+    
